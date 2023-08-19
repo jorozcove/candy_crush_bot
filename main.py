15dffaca2e31be737d54bafaf2f74898f8efc0f7
jorozcove <jorozcove@unal.edu.co>
2023-08-18 22:07:33 -0500

    .

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # from cv2CandySensor import cv2CandySensor
-from sensor2 import cv2CandySensor
+# from sensor2 import cv2CandySensor
+from sensor2_threads import cv2CandySensor
+from time import sleep
 
 if __name__ == '__main__':
     candy_sensor = cv2CandySensor()
     while True:
         print(candy_sensor.get_candy_matrix())
-        input("Enter to continue...")
+        # input("Enter to continue...")
