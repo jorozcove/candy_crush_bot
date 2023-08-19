ea41f55671b036d7082ddf7c47508bcbdb08ccf8
jorozcove <jorozcove@unal.edu.co>
2023-08-18 19:12:16 -0500

    Moved cv2CandySensor to its own file as a class

---
+++ cv2CandySensor.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+import pyscreenshot as ImageGrab
+
+import time
+
+class cv2CandySensor:
+    def __init__(self):
+        start_time = time.time()
+
+        self.top_left = (133, 87)
+        self.cell_size_h = 88
+        self.cell_size_v = 78
+        self.image_paths = 'actual_cells'
+        self.template_images = self.get_templates()
+
+        print(f"Time to load templates: {time.time() - start_time}")
+
+    def get_templates(self, ):
+        candy_colors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
+
+        template_images = {}
+        for color in candy_colors:
+            template_images[color] = cv2.imread(f'candies/{color}/{color}.png', cv2.IMREAD_UNCHANGED)
+            template_images[color+'_sh'] = cv2.imread(f'candies/{color}/{color}_sh.png', cv2.IMREAD_UNCHANGED)
+            template_images[color+'_sv'] = cv2.imread(f'candies/{color}/{color}_sv.png', cv2.IMREAD_UNCHANGED)
+            template_images[color+'_p'] = cv2.imread(f'candies/{color}/{color}_p.png', cv2.IMREAD_UNCHANGED)
+
+        template_images['Ñ'] = cv2.imread(f'candies/Special/special.png', cv2.IMREAD_UNCHANGED)
+        return template_images
+
+    def crop_cell(self, im, i,j):
+        box = (i * self.cell_size_h, j * self.cell_size_v, (i + 1) * self.cell_size_h, (j + 1) * self.cell_size_v)
+        cell_im = im.crop(box)
+        return cell_im
+
+    def get_candy_matrix(self):
+        start_time = time.time()
+
+        bottom_right = (self.top_left[0] + 9 * self.cell_size_h, self.top_left[1] + 9 * self.cell_size_v)
+        im = ImageGrab.grab(bbox=(self.top_left[0], self.top_left[1], bottom_right[0], bottom_right[1]))
+
+        candy_matrix = np.empty((9, 9), dtype=object)
+        for i in range(9):
+            for j in range(9):
+                cell_im = self.crop_cell(im,j,i)
+
+                img_path = f'{self.image_paths}/{i}_{j}.png'
+                cell_im.save(img_path)
+
+                predicted_candy_color = self.classify_candy(img_path, self.template_images)
+                candy_matrix[i, j] = predicted_candy_color[0] + ('_'+predicted_candy_color.split('_')[1] if '_' in predicted_candy_color else '')
+
+        print(f"Time to get candy matrix: {time.time() - start_time}")
+        return candy_matrix
+
+    def classify_candy(self, image_path, template_images):
+        image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+
+        best_match = None
+        best_score = 0.0
+        
+        for variant, template in template_images.items():
+            result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            if max_val > best_score:
+                best_score = max_val
+                best_match = variant
+        
+        return best_match
--- old_sensor.py
+++ old_sensor.py
@@ -1,8 +1,9 @@
 import os
-import cv2
 import numpy as np
 import pyautogui
 import time
+
+#NO SIRVE
 
 '''
 Folder structure:
@@ -16,32 +17,75 @@ candies
         ...
     ...
 '''
-#get list of all candies
-candies_im = []
-for folder in os.listdir('candies'):
-    for file in os.listdir(os.path.join('candies', folder)):
-        candies_im.append(os.path.join('candies', folder, file))
 
-idx = candies_im.index('candies\\green\\g1.png')
-threshhold = 10
-#use pyautogui to find all candies in the board
-for candy in candies_im[idx:idx+1]:
-    positions = []
-    print('Looking for', candy)
+top_left = (133, 87)
+cell_size_h = 88
+cell_size_v = 78
+candy_colors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
 
-    prev_positions = list(pyautogui.locateAllOnScreen(candy, confidence=0.70, region=(top_left[0], top_left[1], 9 * cell_size_h, 9 * cell_size_v )))
+def get_cell_region(i,j):
+    region = (top_left[0] + i * cell_size_h, top_left[1] + j * cell_size_v, cell_size_h, cell_size_v)
+    return region
 
-    #filter out the positions that are too close to each other
-    for pos in prev_positions:
-        if len(positions) == 0:
-            positions.append(pos)
-        else:
-            if min([abs(pos[0] - p[0]) for p in positions]) > threshhold or min([abs(pos[1] - p[1]) for p in positions]) > threshhold:
-                positions.append(pos)
+def main():
+    #get list of all candies
+    candy_paths = {}
+    for color in candy_colors:
+        candy_paths[color] = f'candies/{color}/{color}.png'
+        # candy_paths[color+'_sh'] = f'candies/{color}/{color}_sh.png'
+        # candy_paths[color+'_sv'] = f'candies/{color}/{color}_sv.png'
+        # candy_paths[color+'_p'] = f'candies/{color}/{color}_p.png'
+
+    # candy_paths['Ñ'] = f'candies/Special/special.png'
+
+    #use pyautogui to find all candies in the board
+
+    candy_matrix = np.empty((9, 9), dtype=object)
+    for i in range(9):
+        for j in range(9):
+            print(">>>>",i,j)
+            region = get_cell_region(j,i)
+            region = (region[0], region[1], region[2]+1, region[3]+1)
+            for variant, im_path in candy_paths.items():
+                if pyautogui.locateOnScreen(im_path, grayscale=True, confidence=0.50, region=region):
+                    print(variant, i, j)
+                    candy_matrix[i, j] = variant[0] + ('_'+variant.split('_')[1] if '_' in variant else '')
+                    break
+            
+            print("No candy found")
+                    
+                    # pyautogui.moveTo(candy[0]+40, candy[1]+40)
+                    # pyautogui.click()
+                    # time.sleep(0.5)
+
+    print(candy_matrix)
+
+if __name__ == '__main__':
+    while True:
+        main()
+        input("Enter to continue...")
 
 
-    print('Found', len(positions))
-    for pos in positions:
-        pyautogui.moveTo(pos[0]+40, pos[1]+40)
-        pyautogui.click()
-        time.sleep(0.5)
+
+
+
+
+
+
+
+
+
+#filter out the positions that are too close to each other
+# for pos in prev_positions:
+#     if len(positions) == 0:
+#         positions.append(pos)
+#     else:
+#         if min([abs(pos[0] - p[0]) for p in positions]) > threshhold or min([abs(pos[1] - p[1]) for p in positions]) > threshhold:
+#             positions.append(pos)
+
+
+# print('Found', len(positions))
+# for pos in positions:
+#     pyautogui.moveTo(pos[0]+40, pos[1]+40)
+#     pyautogui.click()
+#     time.sleep(0.5)
