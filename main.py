f025ffbadadff4b4670dbce93e9ef893cad919e7
jorozcove <jorozcove@unal.edu.co>
2023-08-20 19:39:26 -0500

    - Allow script to open the game by itself - Resize template imgs on its own folder based on screen size - sensor now gets values calculated by the board detection - Adeed sleeps to make the main loop start when the game starts

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# from cv2CandySensor import cv2CandySensor
-# from sensor2 import cv2CandySensor
-from sensor2_threads import cv2CandySensor
-from time import sleep
+from sensors.cv2CandySensor import cv2CandySensor
+from sensors.board_detector import get_board_data
+from resizer import resize_images
 
-if __name__ == '__main__':
-    candy_sensor = cv2CandySensor(x = 105, y = 70, cell_size_h = 71, cell_size_v = 63, templates_path='candies_pc')
+from time import sleep
+import os
+
+import subprocess
+
+def main():
+
+    # Open game
+    subprocess.Popen(["Game/ruffle.exe", "Game/CandyCrush.swf"])
+    sleep(10)
+
+    # Get board data, resize images if needed
+    x, y, cell_size_w, cell_size_h = get_board_data() 
+    templates_path = f'candies{cell_size_w}x{cell_size_h}'
+    if not os.path.exists(templates_path):
+        print(f"Resizing images to {cell_size_w}x{cell_size_h}")
+        templates_path = resize_images(w = cell_size_w, h = cell_size_h)
+
+    # Create sensor object
+    candy_sensor = cv2CandySensor(x, y, cell_size_w, cell_size_h, templates_path=templates_path)
+
+    # Wait for game to load
+    sleep(14)
+
     while True:
         print(candy_sensor.get_candy_matrix())
         input("Enter to continue...")
 
+if __name__ == '__main__':
+    main()
+
 # portatil juan
-# x = 133, y = 87, cell_size_h = 88, cell_size_v = 78, templates_path='candies'
+# x = 133, y = 87, cell_size_w = 88, cell_size_h = 78, templates_path='candies'
 
 # pc juan
-# x = 105, y = 70, cell_size_h = 71, cell_size_v = 63, templates_path='candies_pc'
+# x = 105, y = 70, cell_size_w = 71, cell_size_h = 63, templates_path='candies_pc'
+
+
