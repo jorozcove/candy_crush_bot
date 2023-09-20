a37e8090ac39a5d0991c5628dc57f25ef1ec5c53
jorozcove <jorozcove@unal.edu.co>
2023-09-20 16:39:42 -0500

    .

--- main.py
+++ main.py
@@ -26,9 +26,10 @@ def debugImg(img):
 
 def main():
 
-    # game_name = '90_ticks.swf' 
-    # game_name = '1000_ticks.swf'
     game_name = 'CandyCrush.swf'
+    # game_name = '90_ticks.swf' 
+    # game_name = '300_ticks.swf'
+    # game_name = '1000_ticks.swf'
     # game_name = 'candy-crush.exe'
 
     # game_engine = 'ruffle'
